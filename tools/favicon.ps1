149676b0e1244e037ca7da7a3211d5fa903aa826
Adam Byrd <akbyrd@pm.me>
2024-03-04 21:29:45 -0800

    Use ImageMagick for icon conversion instead of Inkscape - Inkscape is too fat to include in the repo. - Still need it for converting text to paths but still happy to reduce dependence on it.

--- tools/favicon.ps1
+++ tools/favicon.ps1
@@ -3,15 +3,22 @@ $assets = "themes\akbyrd\assets"
 function Convert-ImageMagick
 {
 	$magick  = "tools\imagemagick\magick.exe"
-	$input   = "$assets\icon.svg"
-	$output1 = "$assets\favicon.ico"
-	$output2 = "$assets\apple-touch-icon.png"
+	$input   = "$assets\icons\arial.svg"
+	$output1 = "$assets\favicon.svg"
+	$output2 = "$assets\favicon.ico"
+	$output3 = "$assets\apple-touch-icon.png"
 
 	Remove-Item -ErrorAction SilentlyContinue $output1
 	Remove-Item -ErrorAction SilentlyContinue $output2
+	Remove-Item -ErrorAction SilentlyContinue $output3
 
-	& $magick -size 32x32 $input $output1
-	& $magick -size 180x180 $input $output2
+	Copy-Item $input $output1
+	& $magick -size 32x32 -background none $input $output2
+	& $magick -background none `
+		-size 400x400 "radial-gradient:white-#70809080" `
+		-gravity center -crop "180x180+0+0" `
+		-size 140x140 -gravity center $input `
+		-composite $output3
 }
 
 function Convert-Inkscape
@@ -43,12 +50,12 @@ function Convert-Inkscape
 function Convert-RSVG
 {
 	$rsvg   = "tools\rsvg-convert.exe"
-	$input  = "$assets\icon.svg"
+	$input  = "$assets\icons\arial.svg"
 	$output = "$assets\rsvg.png"
 
 	& $rsvg $input -o $output -b white --page-height 48 --page-width 48 --top 17 --left 8
 }
 
-#Convert-ImageMagick
-Convert-Inkscape
+Convert-ImageMagick
+#Convert-Inkscape
 #Convert-RSVG
