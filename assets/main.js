4158d85277c1cd6ca93e2532258b6d4839415a2e
Adam Byrd <akbyrd@pm.me>
2024-03-16 02:32:46 -0700

    Add support for themes - Dark, light, and system - Add an animated svg icon to the header to toggle theme - Workaround broken transitions in firefox. Animating the width of a rect will stall in some cases. Animate rx to nudge it back into gear. - Save selected theme to local storage - Determine theme before body because loading to avoid flicker - Only show icon if javascript is enabled - Ensure system theme is used when javascript is disabled

---
+++ assets/main.js
@@ -0,0 +1,65 @@
+// [x] TODO: default to system preference
+// [x] TODO: save changes if button clicked
+// [x] TODO: apply colors
+// [x] TODO: transition colors
+// [x] TODO: Handle changes when using auto
+// [x] TODO: Do the right thing if no javascript
+// [x] TODO: Hide button if no javascript
+// [x] TODO: javascript isn't loading in multiple browsers (chrome)
+// [x] TODO: transition-duration is wrong in { chrome, edge, brave }
+// [x] TODO: Don't add transition when loading
+// [x] TODO: Consider cycling an integer instead of classes
+// [x] TODO: show button (3 states)
+// [-] TODO: Disable text selection
+// [x] TODO: Consider accessibility
+// [x] TODO: button icons (inline svg?)
+// [-] TODO: Fix pop when clicking quickly
+// [x] TODO: Fix firefox animation
+//
+// [ ] TODO: Move svg to file
+// [ ] TODO: Ensure file is loaded before javascript runs
+// [ ] TODO: Add hover text
+// [ ] TODO: Add hover transition
+// [ ] TODO: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_transitions/Using_CSS_transitions
+// [ ] TODO: https://m2.material.io/design/color/dark-theme.html
+// [ ] TODO: Use typescript?
+
+const theme = {
+	current: 0,
+
+	// Stored. Do not change
+	key: "theme",
+	classes: [
+		"theme-system",
+		"theme-light",
+		"theme-dark",
+	],
+}
+
+function CycleTheme()
+{
+	const previous = theme.current
+	theme.current = (theme.current + 1) % theme.classes.length
+
+	const prevClass = theme.classes[previous]
+	const currClass = theme.classes[theme.current]
+
+	const html = document.getElementsByTagName("html")[0]
+	html.classList.remove(prevClass)
+	html.classList.add(currClass)
+
+	localStorage.setItem(theme.key, theme.current)
+}
+
+function Initialize()
+{
+	theme.current = localStorage.getItem(theme.key)
+
+	const buttons = document.getElementsByClassName("header-theme-button");
+	for (const button of buttons)
+		button.addEventListener("click", CycleTheme)
+}
+
+document.readyState === 'loading'
+	? document.addEventListener("DOMContentLoaded", Initialize)
+	: Initialize()
