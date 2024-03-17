1c1350a2a48791834f3b15dabf6df2fb6f6c39d3
Adam Byrd <akbyrd@pm.me>
2024-03-16 23:59:33 -0700

    Attempt to move theme icon to its own file - Won't actually be using this approach, but want to stash it to refer to later - Transitions don't work. Assuming it's due to classes not applying to the shadow DOM - Would need to ensure the file loads without flickering - Size doesn't obey container. Probably an easy fix. - Still requires styles instead of attributes or `* { unset: all; }` will wipe them out - Can apply colors from css - It's nice to be able to see and work with the icon separately from the site - It's nice to be able to put the transitions in the file itself - The use element requires the href to include an id. - Worse in chromium: mask and hoer don't work with <use>

--- assets/res/main.ts
+++ assets/res/main.ts
@@ -1,29 +1,3 @@
-// [x] TODO: default to system preference
-// [x] TODO: save changes if button clicked
-// [x] TODO: apply colors
-// [x] TODO: transition colors
-// [x] TODO: Handle changes when using auto
-// [x] TODO: Do the right thing if no javascript
-// [x] TODO: Hide button if no javascript
-// [x] TODO: javascript isn't loading in multiple browsers (chrome)
-// [x] TODO: transition-duration is wrong in { chrome, edge, brave }
-// [x] TODO: Don't add transition when loading
-// [x] TODO: Consider cycling an integer instead of classes
-// [x] TODO: show button (3 states)
-// [-] TODO: Disable text selection
-// [x] TODO: Consider accessibility
-// [x] TODO: button icons (inline svg?)
-// [-] TODO: Fix pop when clicking quickly
-// [x] TODO: Fix firefox animation
-// [x] TODO: Use typescript
-// [x] TODO: Make text unselectable
-// [x] TODO: Add hover text
-// [x] TODO: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_transitions/Using_CSS_transitions
-// [x] TODO: https://m2.material.io/design/color/dark-theme.html
-//
-// [ ] TODO: Move svg to file
-// [ ] TODO: Ensure file is loaded before javascript runs
-
 const theme = {
 	current: 0,
 	labels: null as HTMLCollection | null,
