5432cc1469db05ad8005f290843aa24ec251cbdd
Adam Byrd <akbyrd@pm.me>
2024-04-09 23:52:50 -0700

    Add code font

--- tools/fonts.ps1
+++ tools/fonts.ps1
@@ -1,12 +1,33 @@
 $symbolsIn  = "assets\res\SauceCodeProNerdFontPropo-Regular.ttf"
 $symbolsOut = "assets\res\Symbols.woff", "assets\res\Symbols.woff2"
+$codeIn     = "assets\res\SauceCodeProNerdFont-Regular.ttf"
+$codeOut    = "assets\res\Code.woff", "assets\res\Code.woff2"
 
 foreach ($out in $symbolsOut)
 {
+	$ext = Split-Path -Extension $out
+	$ext = $ext.substring(1)
+
 	fonttools `
 		subset `
 		$symbolsIn `
 		--output-file=$out `
-		--verbose `
+		--flavor=$ext `
+		--with-zopfli `
 		--unicodes="uf09e,uf033b,uf02a4,uf0ad1,uf0544,uf01ee"
+}
+
+foreach ($out in $codeOut)
+{
+	$ext = Split-Path -Extension $out
+	$ext = $ext.substring(1)
+
+	fonttools `
+		subset `
+		$codeIn `
+		--output-file=$out `
+		--flavor=$ext `
+		--with-zopfli `
+		--layout-features-="frac,numr,dnom" `
+		--unicodes="u0000-00a0,ufeff,ufffd"
 }
