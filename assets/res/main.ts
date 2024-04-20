0ef783434a943f08d755b271a3879784e8f96e07
Adam Byrd <akbyrd@pm.me>
2024-04-20 16:58:16 -0700

    Hook up copy code button Use 0.0.0.0 for server so both localhost and local ip work

--- assets/res/main.ts
+++ assets/res/main.ts
@@ -29,39 +29,61 @@ function CycleTheme()
 	localStorage.setItem(theme.key, theme.current.toString())
 }
 
+function CopyCode(button: Element)
+{
+	if ("clipboard" in navigator)
+	{
+		const table = button.previousElementSibling!
+		const code = table.querySelector(".lntd:last-of-type code")!
+		if (code.textContent)
+			navigator.clipboard.writeText(code.textContent)
+  }
+}
+
 function Initialize()
 {
 	// Hook up the theme button
-	theme.current = +(localStorage.getItem(theme.key) || 0)
+	{
+		theme.current = +(localStorage.getItem(theme.key) || 0)
 
-	const buttons = document.getElementsByClassName("header-theme-button")
-	for (const button of buttons)
-		button.addEventListener("click", CycleTheme)
+		const buttons = document.getElementsByClassName("header-theme-button")
+		for (const button of buttons)
+			button.addEventListener("click", CycleTheme)
 
-	theme.labels = document.getElementsByClassName("header-theme-label")
-	for (const label of theme.labels)
-		label.innerHTML = theme.values[theme.current].label
+		theme.labels = document.getElementsByClassName("header-theme-label")
+		for (const label of theme.labels)
+			label.innerHTML = theme.values[theme.current].label
+	}
+
+	// Hook up code copy buttons
+	{
+		const buttons = document.getElementsByClassName("code-copy")
+		for (const button of buttons)
+			button.addEventListener("click", () => CopyCode(button));
+	}
 
 	// Fade images in
-	const images = document.getElementsByTagName("img")
-	for (const image of images)
 	{
-		function OnLoad()
-		{
+		const images = document.getElementsByTagName("img")
+		for (const image of images)
+			{
+				function OnLoad()
+				{
 			image.style.opacity = "100%"
 			if (image.naturalWidth)
 				image.classList.add("fade-in")
 		}
 
 		if (image.complete)
-		{
-			OnLoad()
-		}
-		else
-		{
-			image.style.opacity = "0%"
-			image.onload = OnLoad
-			image.onerror = OnLoad
+			{
+				OnLoad()
+			}
+			else
+			{
+				image.style.opacity = "0%"
+				image.onload = OnLoad
+				image.onerror = OnLoad
+			}
 		}
 	}
 }
